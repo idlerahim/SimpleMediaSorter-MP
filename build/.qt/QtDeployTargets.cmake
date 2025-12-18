9911123de5570f06983e05d453b2987503e3c41e
idlerahim <a.rahim29@yahoo.com>
2025-12-19 01:06:37 +0530

    build folder in compressed to a .zip file to resolve dependency issue

--- build/.qt/QtDeployTargets.cmake
+++
@@ -1,3 +0,0 @@
-set(__QT_DEPLOY_TARGET_SimpleMediaSorter-MP_FILE C:/Users/ar/Downloads/TestApp/SimpleMediaSorter-MP/build/SimpleMediaSorter-MP.exe)
-set(__QT_DEPLOY_TARGET_SimpleMediaSorter-MP_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_SimpleMediaSorter-MP_RUNTIME_DLLS C:/Qt/6.10.1/mingw_64/bin/Qt6Widgets.dll;C:/Qt/6.10.1/mingw_64/bin/Qt6Sql.dll;C:/Qt/6.10.1/mingw_64/bin/Qt6Concurrent.dll;C:/Qt/6.10.1/mingw_64/bin/Qt6Gui.dll;C:/Qt/6.10.1/mingw_64/bin/Qt6Core.dll)
